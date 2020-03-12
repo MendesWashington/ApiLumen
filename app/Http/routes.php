a2b0147b99d02cc153e74addb6a7b86b4d2b96c9
mendesswashington <mendesswashington@gmail.com>
2020-03-12 11:08:04 -0300

    Api em Lumen 5.2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,24 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
+//To itializar php -S localhost:8000 -t public
 $app->get('/', function () use ($app) {
-    return $app->version();
+    try {
+        //code...
+        return response()->json($data, 200);//$app->version();
+    } catch (\Throwable $th) {
+        //throw $th;
+    }
+    
+});
+
+$app->get('aluno/{id}/{name}', function($id, $name) use ($app){
+    try {
+        //code...
+        return response()->json($data, 200);
+    } catch (\Throwable $th) {
+        //throw $th;
+        return response()->json('Error in method!', 400);
+    }
+
 });
